782f2972bb7e52869af381b3ee23d2cdef247efa
Mustafa Batar <mustafa.batar@icloud.com>
2022-11-09 03:15:44 +0300

    application completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import {useContext} from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { DebounceInput } from 'react-debounce-input';
 import { AppContext } from './context/appContext';
+import { HighlightOff } from '@mui/icons-material';
+import loadingIcon from './loadingicon.gif';
 
 function App() {
-  const {weather} = useContext(AppContext);
+  const { weather, isLoading, filterText, setFilterText, notFind, isNight } = useContext(AppContext);
+  const [isInputFocused, setIsInputFocused] = useState(false);
 
   return (
-    <div className='bg-cyan-100 min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-l'>
-      {weather?.base}
+    <div className={`min-h-screen bg-gradient-to-r ${isNight ? 'from-slate-900 to-stone-900' : 'from-cyan-500 to-blue-500'} hover:bg-gradient-to-l flex justify-center items-center flex-col p-5`}>
+      <div className='relative'>
+        <DebounceInput minLength={2}
+          debounceTimeout={1000}
+          onChange={event => setFilterText(event.target.value)}
+          value={filterText}
+          onBlur={() => setIsInputFocused(false)}
+          onFocus={() => setIsInputFocused(true)}
+          className={`rounded-md px-12 py-1 text-center ${isInputFocused ? 'opacity-50' : 'opacity-25'} outline-none font-semibold`}
+          placeholder="Search..."
+        />
+        {filterText.length > 0 && <HighlightOff color='action' className='absolute top-2/4 right-1 -translate-y-2/4 cursor-pointer z-10' onClick={() => setFilterText('')} />}
+      </div>
+      <div className="grow flex flex-col justify-center items-center">
+        {isLoading === false ? (
+          <>
+            {
+              notFind ? (<div className={`${isNight === true && 'text-white'} text-center font-semibold`}>Not Find</div>) : (
+                <div className="text-white flex flex-col items-center justify-center">
+                  <div className='text-4xl mb-2'>{weather?.name}</div>
+                  <div className='text-6xl mb-2'>{weather?.main.temp.toFixed(0)}°</div>
+                  <div className='text-xl'>{weather?.weather[0].description}</div>
+                  <div className='text-xl'>Min:{weather?.main.temp_min.toFixed(0)}° Max:{weather?.main.temp_max.toFixed(0)}°</div>
+                  <div><img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`} alt="" /></div>
+                </div>
+              )
+            }
+          </>
+        ) : (<div><img className='w-32' src={loadingIcon} alt="" /></div>)}
+      </div>
     </div>
   );
 }
--- src/context/appContext.js
+++ src/context/appContext.js
@@ -7,7 +7,10 @@ export const AppContext = createContext();
 export const AppContextProvider = ({ children }) => {
 
     const [weather, setWeather] = useState(null);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true);
+    const [filterText, setFilterText] = useState('');
+    const [notFind, setNotFind] = useState(false);
+    const [isNight, setIsNight] = useState(false);
 
     const { coords } = useGeolocated({
         positionOptions: {
@@ -16,22 +19,53 @@ export const AppContextProvider = ({ children }) => {
         userDecisionTimeout: 5000,
     });
 
-    const getWeatherData = async (latitude, longitude) => {
+    useEffect(() => {
+        const filterCity = async (filterData) => {
+            try {
+                setIsLoading(true)
+                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${filterData}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+                setWeather(response.data)
+                setNotFind(false)
+            } catch (error) {
+                console.log(error.response.status)
+                error.response.status === 404 && setNotFind(true)
+
+            } finally {
+                setIsLoading(false)
+            }
+        }
+        filterText === '' ? getWeatherData() : filterCity(filterText);
+    }, [filterText]);
+
+    useEffect(() => {
+        coords !== undefined && getWeatherData();
+    }, [coords]);
+
+    useEffect(() => {
+        if (weather !== null) {
+            const sunRise = new Date(weather.sys.sunrise * 1000).getHours();
+            const sunSet = new Date(weather.sys.sunset * 1000).getHours();
+            const now = new Date().getHours();
+            (now < sunSet && now > sunRise) ? setIsNight(false) : setIsNight(true)
+        }
+    }, [weather]);
+
+    const getWeatherData = async () => {
         try {
             const key = process.env.REACT_APP_WEATHER_API_KEY;
-            const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`);
+            const language = navigator.language.split("-")[0];
+            const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${key}&lang=${language}&units=metric`);
             setWeather(weatherData.data);
-            console.log(weatherData)
+            setNotFind(false)
+            setNotFind(false);
+            setIsLoading(false);
+            console.log(weatherData);
         } catch (error) {
             console.log('error');
         }
     };
 
-    useEffect(() => {
-        coords !== undefined && getWeatherData(coords.latitude, coords.longitude);
-    }, [coords]);
-
-    const value = { weather, isLoading, setIsLoading };
+    const value = { weather, isLoading, setIsLoading, setWeather, getWeatherData, filterText, setFilterText, notFind, isNight };
 
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>
 }
